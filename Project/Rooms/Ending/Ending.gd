654a7bd76fe81ba38ab8979ab64507a049313e3a
Axollyon <Axollyon@users.noreply.github.com>
2021-08-02 02:49:56 -0400

    Ending scene Todo: Ending SFX, restart arrow at end, and final cleanup

---
+++ Project/Rooms/Ending/Ending.gd
@@ -0,0 +1,4 @@
+extends "res://Scripts/Room.gd"
+
+func on_entry():
+	$AnimationPlayer.play("Ending");
--- Project/Rooms/First Floor/First Floor.gd
+++ Project/Rooms/First Floor/First Floor.gd
@@ -3,11 +3,11 @@ extends "res://Scripts/Room.gd"
 var dialogBox = load("res://UI/Dialog Box/Dialog_Player.tscn");
 
 func on_load():
-	if (Global.lastScene == "res://Rooms/Misc/Stairs.tscn" && Global.playerNode):
+	if (Global.lastScene == "res://Rooms/Stairs/Stairs.tscn" && Global.playerNode):
 		Global.playerNode.get_node("AnimationPlayer").play("itoldyoubro");
 
 func on_entry():
-	if (Global.lastScene == "res://Rooms/Misc/Stairs.tscn" && Global.dialogsNode):
+	if (Global.lastScene == "res://Rooms/Stairs/Stairs.tscn" && Global.dialogsNode):
 		var dialogBoxInstance = dialogBox.instance();
 		Global.dialogsNode.add_child(dialogBoxInstance);
 		dialogBoxInstance.dialog = "...You ate shit.";
